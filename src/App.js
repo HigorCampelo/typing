8ada43f027c58936a1e8f992f8c922c1697621c3
Higor Campelo <higorxadrez@hotmail.com>
2021-09-20 20:17:13 -0300

    Word Animation / focus on page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import wordList from "./resources/words.json";
 
 
@@ -16,32 +16,47 @@ const App = () => {
     const [validKeys, setValidKeys] = useState([]);
     const [completedWords, setCompletedWords] = useState([]);
     const [word, setWord] = useState('');
+    const containerRef = useRef(null);
 
     useEffect(() => {
         setWord(getRandomWord());
+        if (containerRef) containerRef.current.focus();
     }, []);
 
     useEffect(() => {
         const wordFromValidKeys = validKeys.join(''); //transforma o validKeys em uma palavra
+        let timeout = null;
+
         if (word && word === wordFromValidKeys) {
             //adicionar word ao completedWord
             //limpar o array validKeys
             //buscar uma nova palavra que não esteja no completedWord
-            setCompletedWords((prev) => [...prev, word]);
-            let newWord = null;
-            do {
-                newWord = getRandomWord();
-            } while (completedWords.includes(newWord));
+            timeout = setTimeout(() => {
+                setCompletedWords((prev) => [...prev, word]);
+                let newWord = null;
+                do {
+                    newWord = getRandomWord();
+                } while (completedWords.includes(newWord));
 
-            setValidKeys([]);
-            setWord(newWord);
+                setValidKeys([]);
+                setWord(newWord);
+
+            }, 600) //meio segundo
+
         }
+
+        return (() => {
+            if (timeout) clearTimeout(timeout);
+        })
     }, [word, validKeys, completedWords]);
 
 
     const handleKeyDown = (e) => {
         e.preventDefault();
-        const { key } = e; //desestruturando com {} se não usar key =e.key
+
+        let key = null; //desestruturando com {key} = e  ou se não usar key =e.key
+        if (e.key !== 'NumLock') key = e.key;
+
         setTypedKeys((prev) => [...prev, key].slice(MAX_TYPED_KEYS * -1));
         //slice(-30) faz com que apareça apenas 30 caracteres na tela
 
@@ -73,15 +88,17 @@ const App = () => {
         const matched = word.slice(0, joineKeys.length);
         const remainder = word.slice(joineKeys.length);
 
+        const matchedClass = (joineKeys === word) ? 'matched completed' : 'matched';
+
         return (
             <>
-                <span className="matched">{matched} </span>
+                <span className={matchedClass}>{matched} </span>
                 <span className="remainder"> {remainder}</span>
             </>
         );
     }
 
-    return (<div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
+    return (<div className="container" tabIndex="0" onKeyDown={handleKeyDown} ref={containerRef}>
         <div className="valid-keys">
             <Word word={word} validKeys={validKeys} />
         </div>
